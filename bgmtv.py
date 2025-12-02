7b7360634e99dc5d8e220e732514d966cbc9ab11
chivehao <chivehao@ikaros.run>
2025-12-02 20:31:33 +0800

    添加番组计划类

---
+++ bgmtv.py
@@ -0,0 +1,15 @@
+from astrbot.api import logger
+import aiohttp
+import os
+import aiofiles
+
+
+class BangumiTvApi():
+    def __init__(self):
+        
+        pass
+
+    def searchSubjectWithId(id: str):
+
+        logger.info(id)
+    
