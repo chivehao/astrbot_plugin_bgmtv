0510aaf46bcb4c9dabf69747f774015565c12c74
chivehao <chivehao@ikaros.run>
2025-12-03 01:26:16 +0800

    实现根据条目ID查询

--- main.py
+++ main.py
@@ -2,33 +2,82 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 from astrbot.api import AstrBotConfig
+import astrbot.api.message_components as Comp
+import openapi_client
+from openapi_client.rest import ApiException
+
+# Defining the host is optional and defaults to https://api.bgm.tv
+# See configuration.py for a list of all supported configuration parameters.
+configuration = openapi_client.Configuration(
+    host = "https://api.bgm.tv"
+)
 
 @register("bgmtv", "chivehao", "AstrBot 插件, 关于Bangumi 番组计划。", "1.0.0")
 class BangumiTvPlugin(Star):
-    access_token = ""
-    user_agent = ""
-
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
-        logger.info("开始初始化...")
+        # logger.info("开始初始化...")
+
         self.config = config
-        self.access_token = self.config.get("access_token", "")
-        self.user_agent = self.config.get("user_agent", "AstrBot-BGMTV-Plugin/2.0")
         self.config.save_config()
-        logger.info("...已完成初始化。")
+        # logger.info("...已完成配置持久化...")
+
+        # logger.info("...已完成配置请求头...")
+
+        # logger.info("...已完成初始化。")
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-
     @filter.command("条目查询")
-    async def helloworld(self, event: AstrMessageEvent):
-        """条目查询 {id}""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。
+    async def searchSubject(self, event: AstrMessageEvent):
+        """条目查询 [角色ID]""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。
+
         user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        cmd = event.message_str.split(maxsplit=1)
+        if len(cmd) < 2:
+            return event.plain_result("格式错误，用法: /条目查询 [角色ID]")
+
+        query = cmd[1].strip()
+
+        # Enter a context with an instance of the API client
+        configuration = openapi_client.Configuration(access_token = self.config.get("access_token", ""))
+        with openapi_client.ApiClient(configuration) as api_client:
+            api_client.user_agent = self.config.get("user_agent", "AstrBot-BGMTV-Plugin/2.0")
+            # logger.info(f"token:{configuration.access_token}\nuser-agent:{api_client.user_agent}")
+            # Create an instance of the API class
+            api_instance = openapi_client.DefaultApi(api_client)
+
+            try:
+
+                if query.isdigit():
+                    subject_id = int(query) # int | 条目 ID
+                    subject = api_instance.get_subject_by_id(subject_id)
+                else:
+                    return event.plain_result("暂未支持关键词查询。")
+                    limit = 1 # int | 分页参数 (optional)
+                    offset = 5 # int | 分页参数 (optional)
+                    search_subjects_request = openapi_client.SearchSubjectsRequest(keyword=query)
+                    api_responses:openapi_client.PagedSubject = api_instance.search_subjects(limit=limit, offset=offset, search_subjects_request=search_subjects_request)
+                    logger.info(api_responses.to_json())
+                    if api_responses.total > 0 :
+                        subject = api_responses.data[0]
+                # logger.info("The response of DefaultApi->get_subject_by_id:\n")
+                # logger.info(subject.name_cn)
+                
+                if not subject:
+                    return event.plain_result("未找到条目。")
+
+                chain = [
+                    Comp.Plain(f"Hello, {user_name}, 你查询的结果如下："),
+                    Comp.Plain(f"名称：{subject.name}\n中文名：{subject.name_cn}\n简介：{subject.summary}\n封面："),
+                    Comp.Image.fromURL(subject.images.large)
+                ]
+                return event.chain_result(chain)
+                # yield event.plain_result(f"Hello, {user_name}, 你查询的结果如下：\n {api_response}!")
+            except Exception as e:
+                logger.error("Exception when calling DefaultApi->get_subject_by_id: %s\n" % e)
+                return event.chain_result("内部异常，请查看日志！！")
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
